599e221a449e9fe29bbfcb44b2938c742d81b54c
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-09-09 11:02:52 -0400

    Commit message

--- Program.cs
+++ Program.cs
@@ -10,21 +10,52 @@ namespace exitMenuOption
     {
         static void Main(string[] args)
         {
+            //declare variables
             string userMenuInput;
+            int startingBalance;
+            int currentBalance;
+            int newIncome;
 
+            
+
+            Console.WriteLine("Welcome" + "\n");
+            Console.WriteLine("\a");
+            
+            
+            //runs the main program
             while (true)
             {
-                Console.WriteLine("This is the begining of the program's run");
+                Console.WriteLine("\n"+"What is your STARTING balance?");
+                startingBalance = int.Parse(Console.ReadLine());
+                Console.WriteLine("What is your NEW income?");
+                newIncome = int.Parse(Console.ReadLine());
+
+                currentBalance = newIncome + startingBalance;
+
+                Console.WriteLine("\n" + "Your starting balance is............." + startingBalance);
+                Console.WriteLine("Your pay for cleaning the attic is..." + newIncome);
+                Console.WriteLine("Your current balance is.............." + currentBalance);
+
                 while (true)
                 {
-                    Console.WriteLine("What should we do next?");
-                    Console.WriteLine("1. Run program again");
-                    Console.WriteLine("2. Exit Program");
+                    Console.WriteLine("\n" + "What should we do next?");
+                    Console.WriteLine("\t" + "1. Run program again");
+                    Console.WriteLine("\t"+"2. Exit Program");
 
                     userMenuInput = Console.ReadLine();
                     if (userMenuInput == "1")
                     {
-                        Console.WriteLine("This is the program looping back around");
+                        Console.WriteLine("\n" + "What is your STARTING balance?");
+                        startingBalance = int.Parse(Console.ReadLine());
+                        Console.WriteLine("What is your NEW income?");
+                        newIncome = int.Parse(Console.ReadLine());
+
+                        currentBalance = newIncome + startingBalance;
+
+                        Console.WriteLine("\n" + "Your starting balance is           " + startingBalance);
+                        Console.WriteLine("Your pay for cleaning the attic is " + newIncome);
+                        Console.WriteLine("Your current balance is            " + currentBalance);
+
                     }
                     else if (userMenuInput == "2")
                     {
@@ -32,7 +63,8 @@ namespace exitMenuOption
                     }
                     else
                     {
-                        Console.WriteLine("this is the invalid option tester");
+                        Console.WriteLine("\n" + "Invalid Option"+"\n" + "Please choose a valid option");
+
                     }
                 }
               
